0cc8c688f82a209841e199e6ad3a2d13f03b2bb4
Lea-Gabay <lea.gabay@tecnico.ulisboa.pt>
2026-09-16 14:51:56 +0100

    correction

--- src/xlmexlab/llm.py
+++ src/xlmexlab/llm.py
@@ -385,7 +385,7 @@ class ModelVLM(BaseModel):
         outputs: list[RequestOutput] = self.model.generate(prompts=new_prompt)
         final_response: str = ""
         for o in outputs:
-            completion += o[1][0][0]
+            completion = o[1][0][0]
             final_response += completion.text
             finish_reason = completion.finish_reason
             print(f"Finish reason: {finish_reason}")
